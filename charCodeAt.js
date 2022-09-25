3e968f3f413618c48806dd7c7a6fb12071d86ac0
KMoore7 <carla.moore3@icloud.com>
2022-09-25 17:43:17 -0400

    Example of charCodeAt() method for JS strings

---
+++ charCodeAt.js
@@ -0,0 +1,19 @@
+// The charCodeAt() method returns the Unicode of the character at a specified index (position) in a string.
+
+// Get the Unicode of the first character in a string:
+let text = "HELLO WORLD";
+let code = text.charCodeAt(0);
+console.log(code)
+//result should be 72 for capital h
+
+// Get the Unicode of the second:
+let text = "HELLO WORLD";
+let code = text.charCodeAt(1);
+//result should be 69 for capital e
+
+// Get the Unicode of the second:
+let text = "HELLO WORLD";
+let code = text.charCodeAt(1);
+//result should be 68 for capital D
+
+
