e840a75913842618865b31b7561da2202240292a
KMoore7 <carla.moore3@icloud.com>
2022-10-10 17:27:08 -0400

    charAt method for JS strings

---
+++ charAt.js
@@ -0,0 +1,21 @@
+// The charAt() method returns the character at a specified index (position) in a string. str.charAt(index)
+
+//Get the first character in a string:
+let text = "HELLO WORLD";
+let letter = text.charAt(0)
+console.log(letter)
+//result should be H
+
+
+// Get the second character in a string:
+let text = "HELLO WORLD";
+let letter = text.charAt(1);
+console.log(letter)
+//result should be E
+
+//Get the last character in a string:
+let text = "HELLO WORLD";
+let letter = text.charAt(text.length-1);
+console.log(letter)
+//result should be D
+
